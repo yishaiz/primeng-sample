df19ee782b5857de7c3c0d3a586aa996f5f3c6ee
yishai <yishai5555@gmail.com>
2018-04-12 16:22:46 +0300

    b6

---
+++ src/app/alltimes/alltimes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alltimes',
+  templateUrl: './alltimes.component.html',
+  styleUrls: ['./alltimes.component.css']
+})
+export class AlltimesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,36 @@ import { SettingsComponent }                from './settings/settings.component'
 import { MenuModule }                       from "primeng/menu";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule }                 from "@angular/common/http";
-import { BrowserAnimationsModule }          from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DashboardComponent }      from './dashboard/dashboard.component';
+import { AlltimesComponent }       from './alltimes/alltimes.component';
+import { TimesheetComponent }      from './timesheet/timesheet.component';
+import { ProjectsComponent }       from './projects/projects.component';
+import { ProfileComponent }        from './profile/profile.component';
+import { RouterModule, Routes }    from "@angular/router";
 // import { PanelSampleComponent } from './panel-sample/panel-sample.component';
 
+
+const appRoutes : Routes = [
+    { path : "", redirectTo : "/dashboard", pathMatch : "full" },
+    { path : "dashboard", component : DashboardComponent },
+    { path : "alltimes", component : AlltimesComponent },
+    { path : "timesheet", component : TimesheetComponent },
+    { path : "projects", component : ProjectsComponent },
+    { path : "profile", component : ProfileComponent },
+    { path : "settings", component : SettingsComponent },
+];
 
 
 @NgModule({
     declarations : [
         AppComponent,
         SettingsComponent,
+        DashboardComponent,
+        AlltimesComponent,
+        TimesheetComponent,
+        ProjectsComponent,
+        ProfileComponent,
         // PanelSampleComponent,
 
     ],
@@ -27,7 +48,8 @@ import { BrowserAnimationsModule }          from "@angular/platform-browser/anim
         MenuModule,
         FormsModule,
         ReactiveFormsModule,
-        HttpClientModule
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes)
     ],
     providers : [],
     bootstrap : [ AppComponent ]
