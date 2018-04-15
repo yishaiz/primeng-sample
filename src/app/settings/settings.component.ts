a4215b76792fb00eb321f627410381d555c0785c
yishai <yishai5555@gmail.com>
2018-04-15 14:59:16 +0300

    b8

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,33 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-// import { Panel } from 'primeng/primeng';
-
 
 @Component({
   selector: 'app-settings',
-  template: ` 
-     <!-- <p-panel header="Title">
-          Content
-      </p-panel>-->
-
-<!--     <p-panel >
-         <p-header>
-             Header content here
-         </p-header>
-         Body Content
-         <p-footer>
-             Footer content here
-         </p-footer>
-     </p-panel>
-     
-     <hr>-->
-
-      <p-panel header="Settings">
-          
-      </p-panel>
-      
-
-  `,
-  styles: []
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
 
@@ -37,3 +13,6 @@ export class SettingsComponent implements OnInit {
   }
 
 }
+
+
+
